ee076d2c7f1300c00e35b4d773bf83a774ca486c
facundorsabia <facundorsabia@gmail.com>
2021-09-17 23:02:12 -0300

    Swiper Slider en Servicios

--- js/index.js
+++ js/index.js
@@ -9,3 +9,11 @@ $("#redesBtn").click(function() {
     $("#logofolioBtn").css("filter", "none").css("z-index", "8").css("width", "169px");
     $("#redesBtn").css("filter", "grayscale(100%)").css("z-index", "7").css("width", "229px");
   });
+
+//Slider Swiper
+
+      var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      });
