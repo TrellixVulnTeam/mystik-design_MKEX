bcf3b129a9c17467e82e41739ac96ae77fe51cf0
facundorsabia <facundorsabia@gmail.com>
2021-09-23 14:47:04 -0300

    PopUp!

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//Slider Swiper JS (libreria) utilizado detector de pantalla agregado
+//Slider Swiper JS utilizado en la sección Servicios
 
 var swiper = new Swiper(".mySwiper", {
   slidesPerView:1,
@@ -20,8 +20,6 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
-
-
 //Switch logofilio vs redes sociales
 
 $("#redesBtn").click(function() {
@@ -33,3 +31,84 @@ $("#redesBtn").click(function() {
     $("#logofolioBtn").css("filter", "none").css("z-index", "8").css("width", "169px");
     $("#redesBtn").css("filter", "grayscale(100%)").css("z-index", "7").css("width", "229px");
   });
+
+
+//Creación de la info del modal de la sección Proyectos, segun el proyecto
+
+var swiper = new Swiper(".mySwiperPopUp", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+$(".rusticopolis").click (function() {
+  $("#popUpSlide2a").hide();
+  $("#popUpSlide3a").hide();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/logo1.png");
+});
+
+$(".nirodha").click (function() {
+  $("#popUpSlide2a").hide();
+  $("#popUpSlide3a").hide();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/logo2.png");
+});
+
+$(".renacer").click (function() {
+  $("#popUpSlide2a").show();
+  $("#popUpSlide3a").show();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/renacer1.jpg");
+  $("#popUpSlide2").attr("src", "imagenes/logofolio/renacer2.jpg");
+  $("#popUpSlide3").attr("src", "imagenes/logofolio/renacer3.jpg");
+});
+
+
+$(".credimoney").click (function() {
+  $("#popUpSlide2a").hide();
+  $("#popUpSlide3a").hide();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/logo4.png");
+});
+
+$(".xy").click (function() {
+  $("#popUpSlide2a").hide();
+  $("#popUpSlide3a").hide();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/logo5.webp");
+});
+
+$(".bombadil").click (function() {
+  $("#popUpSlide2a").show();
+  $("#popUpSlide3a").show();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/bombadil1.jpg");
+  $("#popUpSlide2").attr("src", "imagenes/logofolio/bombadil2.jpg");
+  $("#popUpSlide3").attr("src", "imagenes/logofolio/bombadil3.jpg");
+});
+
+$(".epoxy").click (function() {
+  $("#popUpSlide2a").hide();
+  $("#popUpSlide3a").hide();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/logo7.webp");
+});
+
+$(".click").click (function() {
+  $("#popUpSlide2a").show();
+  $("#popUpSlide3a").show();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/click1.jpg");
+  $("#popUpSlide2").attr("src", "imagenes/logofolio/click2.png");
+  $("#popUpSlide3").attr("src", "imagenes/logofolio/click3.png");
+});
+
+
+$(".haini").click (function() {
+  $("#popUpSlide2a").hide();
+  $("#popUpSlide3a").hide();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/logo9.webp");
+});
+
+
+$(".candys").click (function() {
+  $("#popUpSlide2a").hide();
+  $("#popUpSlide3a").hide();
+  $("#popUpSlide1").attr("src", "imagenes/logofolio/logo10.webp");
+});
+
